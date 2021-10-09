0b63950cf1f1b5171e6bca78d9a63b31985a81c0
Kaleidosport <lambert.ant14@gmail.com>
2021-10-10 00:36:33 +0200

    Adding Test1's 1st part

--- 3.Jasmine/3.1.Test1/questions.js
+++ 3.Jasmine/3.1.Test1/questions.js
@@ -8,35 +8,42 @@ let remplaceECar = (texte) => {
     return text;
 }
 let concatString = (texte1, texte2) => {
-
+    return texte1.concat("", texte2)
 }
 let afficherCar5 =  (texte) => {
-
+    return texte.substring(4, 5)
 }
 let afficher9Car =  (texte) => {
-
+    return texte.substring(0, 9)
 }
 let majusculeString =  (texte) => {
-
+    return texte.toUpperCase()
 }
 let minusculeString =  (texte) => {
-
+    return texte.toLowerCase()
 }
 let SupprEspaceString =  (texte) => {
-
+    return texte.trim()
+    // Or return texte.slice(1, -1)
 }
 let IsString =  (texte) => {
-
+    return typeof texte === "string" ? true : false
 }
 
 let AfficherExtensionString =  (texte) => {
-
+    return texte.substring(texte.lastIndexOf(".") + 1)
+    /**
+     * Remove + 1 to get ".jpg" as an answer while texte.lastIndexOf = 25
+     * return texte.split(".").pop() works as well
+     */
 }
-let NombreEspaceString =  (texte) => {
 
+let NombreEspaceString =  (texte) => {
+    return texte.match(/\s/g).length
+    // Gonna have to ask Arnaud why texte.search(/\s/g) returns 4 instead of 5
 }
 let InverseString =  (texte) => {
-
+    return texte.split("").reverse().join("")
 }
 
 /**
