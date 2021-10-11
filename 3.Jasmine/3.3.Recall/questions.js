12823f4122d7f9aab7518b18eeca5ac79fab3064
Kaleidosport <lambert.ant14@gmail.com>
2021-10-11 14:55:27 +0200

    Anagrams & Titleize still pending

--- 3.Jasmine/3.3.Recall/questions.js
+++ 3.Jasmine/3.3.Recall/questions.js
@@ -1,135 +1,79 @@
-let selectElementsStartingWithA = (array) => {
-    return 'Write your method here';
-}
+let selectElementsStartingWithA = (array) => array.filter((fruit) => fruit.startsWith("a"))
 
-let selectElementsStartingWithVowel = (array) => {
-    return 'Write your method here';
-}
+let selectElementsStartingWithVowel = (array) => array.filter(vowel => /^[aeiou]/i.test(vowel))
 
-let removeNullElements = (array) => {
-    return 'Write your method here';
-}
+let removeNullElements = (array) => array.filter(element => element !== null)
 
-let removeNullAndFalseElements = (array) => {
-    return 'Write your method here';
-}
+let removeNullAndFalseElements = (array) => array.filter(element => (element !== null && element !== false))
 
-let reverseWordsInArray = (array) => {
-    return 'Write your method here';
-}
+let reverseWordsInArray = (array) => array.map(word => word.split("").reverse().join(""))
 
-let everyPossiblePair = (array) => {
-    return 'Write your method here';
-}
+let everyPossiblePair = (array) => array.sort().map((character, index, sortedArray) => sortedArray.filter(char => char !== character)).reverse()
 
-let allElementsExceptFirstThree = (array) => {
-    return 'Write your method here';
-}
+let allElementsExceptFirstThree = (array) => array.splice(3)
 
-let addElementToBeginning = (array, element) => {
-    return 'Write your method here';
-}
+let addElementToBeginning = (array, element) => (array.unshift(element), array)
 
-let sortByLastLetter = (array) => {
-    return 'Write your method here';
-}
+let sortByLastLetter = (array) => array.sort((a, b) => a.charCodeAt(a.length -1) - b.charCodeAt(b.length - 1))
+// https://www.codegrepper.com/code-examples/javascript/javascript+sort+array+by+last+letters
 
-let getFirstHalf = (string) => {
-    return 'Write your method here';
-}
+let getFirstHalf = (string) => string.substring(0, Math.round(string.length / 2))
 
-let makeNegative = (number) => {
-    return 'Write your method here';
-}
+let makeNegative = (number) => -Math.abs(number)
 
-let numberOfPalindromes = (array) => {
-    return 'Write your method here';
-}
+let numberOfPalindromes = (array) => array.filter(word => word === word.split("").reverse().join("")).length
+// Or array.filter(word => word === [...word].reverse().join('')).length
 
-let shortestWord = (array) => {
-    return 'Write your method here';
-}
+let shortestWord = (array) => [...array.sort((a, b) => a.length - b.length)][0]
 
-let longestWord = (array) => {
-    return 'Write your method here';
-}
+let longestWord = (array) => [...array.sort((a, b) => b.length - a.length)][0]
 
-let sumNumbers = (array) => {
-    return 'Write your method here';
-}
+let sumNumbers = (array) => array.reduce((a, b) => a + b, 0)
 
-let repeatElements = (array) => {
-    return 'Write your method here';
-}
+let repeatElements = (array) => [...array, ...array]
 
-let stringToNumber = (string) => {
-    return 'Write your method here';
-}
+let stringToNumber = (string) => Number(string)
 
-let calculateAverage = (array) => {
-    return 'Write your method here';
-}
+let calculateAverage = (array) => array.reduce((a, b) => a + b, 0) / array.length
 
-let getElementsUntilGreaterThanFive = (array) => {
-    return 'Write your method here';
-}
+let getElementsUntilGreaterThanFive = (array) => array.slice(0, array.find((element) => element > 5))
 
-let convertArrayToObject = (array) => {
-    return 'Write your method here';
-}
+let convertArrayToObject = (array) => Object.fromEntries(array.map((element, index, arr) => (index % 2 === 1) ? [arr[index - 1], arr[index]] : null).filter(element => element !== null))
 
-let getAllLetters = (array) => {
-    return 'Write your method here';
-}
+let getAllLetters = (array) => [...new Set(array)].join("").split("").sort()
 
 let swapKeysAndValues = (object) => {
-    return 'Write your method here';
+    let newObject = {}
+    for (let key in object) newObject[object[key]] = key
+    return newObject
+    // https://stackoverflow.com/questions/23013573/swap-key-with-value-in-object
+    // Cleaner: Object.fromEntries(Object.entries(object).map(([key, value]) => [value, key]))
 }
 
-let sumKeysAndValues = (object) => {
-    return 'Write your method here';
-}
+let sumKeysAndValues = (object) => Object.entries(object).join(",").split(",").reduce(((a, b) => Number(a) + Number(b)))
 
-let removeCapitals = (string) => {
-    return 'Write your method here';
-}
+let removeCapitals = (string) => string.replace(/[A-Z]/g, "")
 
-let roundUp = (number) => {
-    return 'Write your method here';
-}
+let roundUp = (number) => Math.ceil(number)
 
-let formatDateNicely = (date) => {
-    return 'Write your method here';
-}
+let formatDateNicely = (date) => date.toLocaleDateString("du-Be")
 
-let getDomainName = (string) => {
-    return 'Write your method here';
-}
+let getDomainName = (string) => string.split("@").pop().split(".com").shift()
 
 let titleize = (string) => {
     return 'Write your method here';
 }
 
-let checkForSpecialCharacters = (string) => {
-    return 'Write your method here';
-}
+let checkForSpecialCharacters = (string) => /\W/g.test(string)
 
-let squareRoot = (number) => {
-    return 'Write your method here';
-}
+let squareRoot = (number) => Math.sqrt(number)
 
-let factorial = (number) => {
-    return 'Write your method here';
-}
+let factorial = (number) => number <= 1 ? 1 : (number * factorial (number - 1))
 
 let findAnagrams = (string) => {
     return 'Write your method here';
 }
 
-let convertToCelsius = (number) => {
-    return 'Write your method here';
-}
+let convertToCelsius = (number) => Math.round((number - 32) / 1.8)
 
-let letterPosition = (array) => {
-    return 'Write your method here';
-}
+let letterPosition = (array) => array.map(element => element.toLowerCase().charCodeAt(0) - 96)
