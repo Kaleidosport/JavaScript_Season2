9c39a24220630c06d9015a330717858bc89d8370
Kaleidosport <lambert.ant14@gmail.com>
2021-10-14 23:03:24 +0200

    Added a few alternatives here and there

--- 3.Jasmine/3.3.Recall/questions.js
+++ 3.Jasmine/3.3.Recall/questions.js
@@ -1,6 +1,8 @@
 let selectElementsStartingWithA = (array) => array.filter((fruit) => fruit.startsWith("a"))
+// Or array.filter(fruit => fruit[0] === "a")
 
 let selectElementsStartingWithVowel = (array) => array.filter(vowel => /^[aeiou]/i.test(vowel))
+// Or array.filter(word => word.charAt(0).match(/[aeyuio]+/))
 
 let removeNullElements = (array) => array.filter(element => element !== null)
 
@@ -11,6 +13,7 @@ let reverseWordsInArray = (array) => array.map(word => word.split("").reverse().
 let everyPossiblePair = (array) => array.sort().map((character, index, sortedArray) => sortedArray.filter(char => char !== character)).reverse()
 
 let allElementsExceptFirstThree = (array) => array.splice(3)
+// Or array.filter((item, index) => index > 2)
 
 let addElementToBeginning = (array, element) => (array.unshift(element), array)
 // Or [element, ...array]
@@ -29,7 +32,7 @@ let shortestWord = (array) => [...array.sort((a, b) => a.length - b.length)][0]
 // Or array.reduce((a, b) => a.length < b.length ? a : b)
 
 let longestWord = (array) => [...array.sort((a, b) => b.length - a.length)][0]
-//Or array.reduce((a, b) => a.length > b.length ? a : b)
+// Or array.reduce((a, b) => a.length > b.length ? a : b)
 
 let sumNumbers = (array) => array.reduce((a, b) => a + b, 0)
 
@@ -40,6 +43,7 @@ let stringToNumber = (string) => Number(string)
 let calculateAverage = (array) => array.reduce((a, b) => a + b, 0) / array.length
 
 let getElementsUntilGreaterThanFive = (array) => array.slice(0, array.find((element) => element > 5))
+// Or array.slice(0, array.indexOf(6)) but that's too cheap imo
 
 let convertArrayToObject = (array) => Object.fromEntries(array.map((element, index, arr) => (index % 2 === 1) ? [arr[index - 1], arr[index]] : null).filter(element => element !== null))
 // Or let convertArrayToObject = (array) => array.reduce((acc, current, index, arr) => (index % 2 ? acc[arr[index - 1]] = current : acc[current], acc), {})
@@ -64,6 +68,7 @@ let roundUp = (number) => Math.ceil(number)
 let formatDateNicely = (date) => date.toLocaleDateString("du-Be")
 
 let getDomainName = (string) => string.split("@").pop().split(".com").shift()
+// Or string.slice(string.indexOf("@") + 1, string.lastIndexOf("."))
 
 let titleize = (string) => string.replace(/(^[^ ])|(?<= )([^ at])|(?<=\. )([^ ])/g, (element) => element.toUpperCase())
 
@@ -95,3 +100,5 @@ let findAnagrams = (string) => {
 let convertToCelsius = (number) => Math.round((number - 32) / 1.8)
 
 let letterPosition = (array) => array.map(element => element.toLowerCase().charCodeAt(0) - 96)
+// Or using .map() method after specifying the whole alphabet through a dedicated variable...
+// Or maybe through yet another RegEx? Too exhausted to look it up
